25d4c02bebc7a1db047054387e97d8b68bfa3916
Orlando <orlandomoncada610@gmail.com>
2020-02-18 15:55:31 -0500

    arreglo login

--- codigoPunto_Qualite/js/popup.js
+++ codigoPunto_Qualite/js/popup.js
@@ -3,13 +3,35 @@ var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
-btnAbrirPopup.addEventListener('click', function(){
+btnAbrirPopup.addEventListener('click', function () {
 	overlay.classList.add('active');
 	popup.classList.add('active');
 });
 
-btnCerrarPopup.addEventListener('click', function(e){
+btnCerrarPopup.addEventListener('click', function (e) {
 	e.preventDefault();
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
+});
+
+$(".toggle-password1").click(function () {
+
+	$(this).toggleClass("fa-eye fa-eye-slash");
+	var input = $($(this).attr("toggle"));
+	if (input.attr("type") == "password") {
+		input.attr("type", "text");
+	} else {
+		input.attr("type", "password");
+	}
+});
+
+$(".toggle-password2").click(function () {
+
+	$(this).toggleClass("fa-eye fa-eye-slash");
+	var input = $($(this).attr("toggle"));
+	if (input.attr("type") == "password") {
+		input.attr("type", "text");
+	} else {
+		input.attr("type", "password");
+	}
 });
