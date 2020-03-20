191027429c4f8aed74d9bcd2ef521540bc99030d
Alejandro <ifredomendoza@gmail.com>
2020-03-20 15:24:32 -0500

    Modulos

--- codigoPunto_Qualite/hoja_de_vida.php
+++ codigoPunto_Qualite/hoja_de_vida.php
@@ -1,84 +1,8 @@
-<?php
-include_once "php-objects/conexion.inc.php";
-include_once "php-objects/repositorio.php";
-include_once "php-objects/usuario.inc.php";
-
-?>
-<!DOCTYPE html>
-<html lang="es">
-
 <head>
-    <!-- Bootstrap 4 -->
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/lux1.css">
-    <title>Registro</title>
-
-    <!-- Scrool reveal -->
-    <script src="https://unpkg.com/scrollreveal"></script>
-
-    <script>
-        function habilitar(value) {
-            if (value == true) {
-                // habilitamos
-                document.getElementById("universidad").disabled = false;
-                document.getElementById("carrera").disabled = false;
-                document.getElementById("promedio").disabled = false;
-            } else if (value == false) {
-                // deshabilitamos
-                document.getElementById("universidad").disabled = true;
-                document.getElementById("carrera").disabled = true;
-                document.getElementById("promedio").disabled = true;
-            }
-        }
-    </script>
-
-</head>
-
-
-
-<body>
-
-
-
-    <!--NAVBAR-->
-
-    <nav style="z-index:40!important;" class=" navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-
-        <a class="navbar-brand" href="../index.php">Punto Qualité</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarColor01">
-
-
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown show ">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Bebé y maternidad</a>
-                        <a class="dropdown-item" href="#">Fitness</a>
-                        <a class="dropdown-item" href="#">Cuidado personal</a>
-                </li>
-                <li class="nav-item ">
-                    <a class="nav-link" href="our.php">Nosotros</a>
-                </li>
-                <li class="nav-item ">
-                    <a class="nav-link" href="our.php">Contactanos</a>
-                </li>
-            </ul>
-
-
-        </div>
-    </nav>
-    <!--FIN NAVBAR-->
+    <title>Hoja de vida</title>
+<?php
+include_once $_SERVER['DOCUMENT_ROOT']."/PFW/codigoPunto_Qualite/modulos/navbar.inc.php";
+?>
     <div class="container cont-forms">
         <br />
         <br />
@@ -197,5 +121,7 @@ include_once "php-objects/usuario.inc.php";
     <script src='https://www.google.com/recaptcha/api.js'></script>
 </body>
 
-
-</html>
+<?php
+include_once $_SERVER['DOCUMENT_ROOT']."/PFW/codigoPunto_Qualite/modulos/b_scripts.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/PFW/codigoPunto_Qualite/modulos/footer.php";
+?>
---
+++ codigoPunto_Qualite/modulos/footer.php
@@ -0,0 +1,63 @@
+
+  <!-- Inicio footer -->
+  <footer class="container py-5">
+    <div class="row">
+      <div class="col-6 col-md">
+        <h5>Sedes</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="https://www.google.com/maps/place/Cl.+32+%2326-133+a+26-55,+Bucaramanga,+Santander/@7.1243538,-73.1207863,17z/data=!3m1!4b1!4m5!3m4!1s0x8e681563c7a8a6a9:0x3d64f76f728c5761!8m2!3d7.124348!4d-73.1185974">Bucaramanga</a></li>
+          <li><a class="text-muted" href="https://www.google.com/maps/place/Cra.+26b+%2334-2+a+34-124,+Floridablanca,+Santander/@7.0688534,-73.1053061,17z/data=!3m1!4b1!4m5!3m4!1s0x8e683f71d7396e3d:0xab772f025f267d0e!8m2!3d7.0688528!4d-73.1031178">Floridablanca</a></li>
+          <li><a class="text-muted" href="https://www.google.com/maps/place/Cl.+16+%231217,+Duitama,+Boyac%C3%A1/@5.8304793,-73.0360097,18z/data=!3m1!4b1!4m13!1m7!3m6!1s0x8e6a3f0ca12f6173:0x6a043a55d2f7de6e!2sDuitama,+Boyac%C3%A1!3b1!8m2!3d5.8268951!4d-73.0329273!3m4!1s0x8e6a3f0bf9e90dfd:0xf53a1436160af546!8m2!3d5.8304766!4d-73.0349154">Duitama</a></li>
+          <li><a class="text-muted" href="https://www.google.com/maps/place/Cl.+48+%2326-2+a+26-154,+Gir%C3%B3n,+Santander/@7.0760531,-73.171821,17z/data=!3m1!4b1!4m13!1m7!3m6!1s0x8e683e90912b50ff:0xe9aa9708e4b018c6!2sGir%C3%B3n,+Santander!3b1!8m2!3d7.0739678!4d-73.1692652!3m4!1s0x8e683e8f4dbd17a9:0x616b6bb50cfb1ace!8m2!3d7.0760478!4d-73.1696323">Giron</a></li>
+          <li><a class="text-muted" href="https://www.google.com/maps/place/Calle+22+A,+Yopal,+Casanare/@5.3386884,-72.393591,17z/data=!3m1!4b1!4m13!1m7!3m6!1s0x8e6b0dca93add7cb:0x46dfbc4a24770cfe!2sYopal,+Casanare!3b1!8m2!3d5.348903!4d-72.400523!3m4!1s0x8e6b0db213fee0a9:0xc53c66b0fabe5390!8m2!3d5.3386831!4d-72.3914022">Yopal</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md" id="contacto">
+        <h5>Contacto</h5>
+        <p class="text-muted">
+          Andrés Santiago Castro<br>
+          Fredy Alejandro Mendoza<br>
+          Orlando Alberto Moncada<br>
+          Andres Felipe Uribe<br>
+        </p>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Acerca de Punto Qualité</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="codigoPunto_Qualite/our.php#visio n">Vision</a></li>
+          <li><a class="text-muted" href="codigoPunto_Qualite/our.php#mision">Mision</a></li>
+          <li><a class="text-muted" href="codigoPunto_Qualite/our.php#marca">Nuestra marca</a></li>
+          <li><a class="text-muted" href="codigoPunto_Qualite/our.php#trabajo">Unetenos</a></li>
+        </ul>
+      </div>
+      <!-- Redes cociales -->
+      <div class="col-3 col-md">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false">
+          <title>Product</title>
+          <circle cx="12" cy="12" r="10" />
+          <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
+        </svg>
+        <small class="d-block mb-3 text-muted">&copy; 2020</small>
+        <div class="row">
+          <div class="col-3 col-md">
+            <a href="https://www.facebook.com/" target="_blank"><img src="/PFW/datosPunto_Qualite/img/facebook1.png" style="width: 100%;"></a>
+          </div>
+          <div class="col-3 col-md">
+            <a href="https://www.instagram.com/" target="_blank"><img src="/PFW/datosPunto_Qualite/img/instagram1.png" style="width: 100%;"></a>
+          </div>
+          <div class="col-3 col-md">
+            <a href="https://twitter.com/PuntoQualite/" target="_blank"><img src="/PFW/datosPunto_Qualite/img/twitter1.png" style="width: 100%;"></a>
+          </div>
+          <div class="col-3 col-md">
+            <a href="https://www.youtube.com/" target="_blank"><img src="/PFW/datosPunto_Qualite/img/youtube1.png" style="width: 100%;"></a>
+          </div>
+        </div>
+      </div>
+      <!-- Fin redes -->
+    </div>
+
+  </footer>
+</body>
+<script src="/PFW/codigoPunto_Qualite/js/popup.js"></script>
+
+</html>
