e340e093519af8cdc4806d5019e7ffee4bf53b2b
Alejandro <ifredomendoza@gmail.com>
2020-03-22 14:27:35 -0500

    Se agregaron unas cositas

--- codigoPunto_Qualite/ciudad.php
+++ codigoPunto_Qualite/ciudad.php
@@ -1,12 +1,15 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/20192B105/codigoPunto_Qualite/modulos/navbar.inc.php";
 ?>
+<?php
+$saber = 1;
+?>
 
   <div class="row" style="padding-top: 6em">
-    
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/20192B105/codigoPunto_Qualite/modulos/nav.php";
 ?>
+
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
       <div style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;" class="chartjs-size-monitor">
         <div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
@@ -45,10 +48,20 @@ include_once $_SERVER['DOCUMENT_ROOT']."/20192B105/codigoPunto_Qualite/modulos/n
             ?>
           </tbody>
         </table>
-      </div>
-    
-    
-    
+        <h4 style="margin-top:10px">Agregar ciudad</h4>
+        <table class="table table-striped table-sm">
+        <div>
+        <tr>
+            <td><input name="id" class="form-control" id="id"  value=<?php echo count(repositorioFunciones::obtener_ciudades($conex)) + 1; ?> required disabled> </td>
+          <form action="<?php echo ($_SERVER['PHP_SELF']); ?>" method="POST">
+              <td><input name="ciudad" class="form-control" id="ciudad"  placeholder="Ciudad" required></td>
+              <td></td>
+        </tr>
+            <tr> <td><button type="submit" class="btn btn-primary form-group" id="agregar" name="commit">Agregar</button></td></tr>
+            </form>
+            </div>
+        </table>
+            </div>
     </main>
   </div>
 
