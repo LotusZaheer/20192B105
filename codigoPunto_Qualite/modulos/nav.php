330b35794eb25709ad87ef86a5266cbe05c53c4d
LotusZaheer <andresfelipeuribe11@gmail.com>
2020-03-23 22:41:17 -0500

    Una x bien mela, gordita

--- codigoPunto_Qualite/modulos/nav.php
+++ codigoPunto_Qualite/modulos/nav.php
@@ -56,8 +56,8 @@
           <li class="nav-item">
             <a class="nav-link" href="/20192B105/codigoPunto_Qualite/editar_datos/eliminar_base.php">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2">
-                <line x1="5" y1="19" x2="19" y2="5"></line>
-                <line x1="5" y1="5" x2="19" y2="19"></line>
+                <line stroke-linejoin="round" stroke="black" stroke-width="3" x1="5" y1="19" x2="19" y2="5"></line>
+                <line stroke-linejoin="round" stroke="black" stroke-width="3" x1="5" y1="5" x2="19" y2="19"></line>
               </svg>
               Eliminar base de datos
             </a>
